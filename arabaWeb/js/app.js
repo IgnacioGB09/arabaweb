abbead6627cd34610444adb4ac0dd145c4f078fa
Ignacio <125318844+IgnacioGB09@users.noreply.github.com>
2023-07-09 16:15:36 -0600

    Add files via upload

---
+++ arabaWeb/js/app.js
@@ -0,0 +1,11 @@
+let carrusel = document.querySelector('.contenedorTarjetas');
+let botonI = document.getElementById('izquierda');
+let botonD = document.getElementById('derecha');
+
+botonD.addEventListener('click', () =>{
+    carrusel.scrollLeft += carrusel.offsetWidth;
+})
+
+botonI.addEventListener('click', () =>{
+    carrusel.scrollLeft -= carrusel.offsetWidth;
+})
