f6c9fc1bd49f4f49a4dbf79297563734965bbdb1
Ignacio <125318844+IgnacioGB09@users.noreply.github.com>
2024-08-18 15:38:32 -0600

    Update app.js

--- arabaWeb/js/app.js
+++ arabaWeb/js/app.js
@@ -1,1 +1,12 @@
 
+let carrusel = document.querySelector('.contenedorTarjetas');
+let botonI = document.getElementById('izquierda');
+let botonD = document.getElementById('derecha');
+
+botonD.addEventListener('click', () =>{
+    carrusel.scrollLeft += carrusel.offsetWidth;
+})
+
+botonI.addEventListener('click', () =>{
+    carrusel.scrollLeft -= carrusel.offsetWidth;
+})
